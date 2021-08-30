ca7c6c3bcb63614294803cd65ccca6e36abb9af4
brianlam37 <brianlam37@users.noreply.github.com>
2021-08-30 16:41:11 -0700

    Attempt to fix error on production build, caused by client side routing

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
-const listRouter = require('./routes/list');
+// const listRouter = require('./routes/list');
 
 app.use(cors());
-app.use('/api/lists', listRouter);
+// app.use('/api/lists', listRouter);
+
 app.use(express.static('build'));
+app.get('*', (req, res) => {
+	res.sendFile('index.html', {
+		root: path.join(__dirname, '/build'),
+	});
+});
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
